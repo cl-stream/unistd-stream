d91a80c97d7c700baf796745592783531cb2a4e5
Thomas de Grivel <thoxdg@gmail.com>
2017-06-17 21:31:49 +0200

    more tests

--- test.lisp
+++ test.lisp
@@ -9,6 +9,16 @@
 
 (in-package :fd-stream.test)
 
+(let ((s (fd-output-stream 1)))
+  (write s 13)
+  (dotimes (i 10)
+    (write s (+ 48 i)))
+  (write s 13)
+  (flush s))
+
+(let ((s (fd-input-stream 0)))
+  (read s))
+
 (with-stream (s (open-file "/tmp/test"))
   (dotimes (i 100)
     (write s i)))
